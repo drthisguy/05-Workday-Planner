dedfcfc7b93411c8be40485de25e0bc236d98694
My Lord <paget@sapling-inc.local>
2019-12-17 15:47:57 -0500

    coloring function built

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,26 +2,22 @@ $(document).ready(function() {
 //date display for the heading.
 $('#currentDay').text(moment().format('dddd MMMM Do'));
 
+var schedule;
 //load schedule from LS
 getScheduleFromLS();
 
 //load schedule from LS
 function getScheduleFromLS() {
-  var schedule;
   if (localStorage.getItem("schedule") === null) {
     schedule = [];
   } else {
     schedule = JSON.parse(localStorage.getItem("schedule"));
   }
-console.log(schedule);
-
   $.each(schedule, function (index, item) {
   $(`#${item.time}`).children('.description').append(item.plan);
-        
 })}
 
 function storeInLS(plan) {
-    var schedule;
     if (localStorage.getItem("schedule") === null) {
       schedule = [];
     } else {
@@ -31,18 +27,33 @@ function storeInLS(plan) {
     schedule.push(plan);
     //set back in LS
     localStorage.setItem("schedule", JSON.stringify(schedule));
+ }
 
- };
+function colorizor() {
+    var timeOfDay = moment().hour();
+    console.log(timeOfDay);
 
+    $.each(schedule, function (index, item) {
+        var timeSlot = index + 9
+        console.log(schedule);
+        
+        if ((timeOfDay > timeSlot)) {
+            $(`#${item.time}`).children('.description').addClass('past');
+        } else if (timeOfDay < timeSlot) {
+            $(`#${item.time}`).children('description').attr('.future');
+        } else {
+            ;
+        }
+})};
+
+
+colorizor();
 $('.saveBtn').on('click', function() {
     var scheduleItem  = {
         'plan' : $(this).siblings('.description').val(),
         'time' : $(this).parent().attr('id') //this is the hour itself
     }
-    storeInLS(scheduleItem);
-
-    console.log(scheduleItem);
-    
+    storeInLS(scheduleItem); 
 })
 
 })
